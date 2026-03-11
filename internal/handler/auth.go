f8538f3bb50a2358bd2054b6730a1f723293a6cb
walker <zw37520@gmail.com>
2026-03-12 00:20:51 +0800

    debug: try short token for /me/accounts

--- internal/handler/auth.go
+++ internal/handler/auth.go
@@ -157,6 +157,18 @@ func (h *AuthHandler) MetaConnect(c *gin.Context) {
 
 	// Step 3: Get user's Facebook Pages
 	h.Tokens.UserToken = longToken
+
+	// Debug: also try with short token
+	shortPages, shortErr := h.client.GetUserPages(ctx, req.AccessToken)
+	if shortErr != nil {
+		log.Printf("[Auth/Connect] short token /me/accounts: err=%v", shortErr)
+	} else {
+		log.Printf("[Auth/Connect] short token /me/accounts: %d pages", len(shortPages))
+		for _, p := range shortPages {
+			log.Printf("[Auth/Connect]   page: %s (%s)", p.Name, p.ID)
+		}
+	}
+
 	pages, err := h.client.GetUserPages(ctx, longToken)
 	if err != nil {
 		log.Printf("[Auth/Connect] get pages failed (may lack permissions): %v", err)
